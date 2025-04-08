cc5d492f0052fb2a6469722969636c2e77cd9f4b
victorpaularony <smallypauls@gmail.com>
2025-04-08 19:16:48 +0300

    function isStringContainedIn that returns true if the string subString is contained s string.

---
+++ StringContains/StringContains.java
@@ -0,0 +1,17 @@
+public class StringContains {
+    public static boolean isStringContainedIn(String subString, String s) {
+        return s.contains(subString);
+    }
+
+    //tester
+    public static void main(String[] args) {
+        String subString = "hell";
+        String s = "hello";
+
+        String s1 = "Bonjour";
+        String s2 = "hello World !";
+
+        System.out.println(isStringContainedIn(subString, s));
+        System.out.println(isStringContainedIn(s1, s2));
+    }
+}
