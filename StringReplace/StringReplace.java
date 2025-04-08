e237df2f9a1c4bb1a2483d189e3eef846f8199e2
victorpaularony <smallypauls@gmail.com>
2025-04-08 19:49:03 +0300

    function called StringReplace

--- StringReplace/StringReplace.java
+++ StringReplace/StringReplace.java
@@ -6,6 +6,13 @@ public class StringReplace {
         return s.replace(target, replacement);
     }
 
+    public static String replace(String s, char target, char replacement) {
+        if (s == null) {
+            return s;
+        }
+        return s.replace(target, replacement);
+    }
+
     // tester
     public static void main(String[] args) {
         String s = "victor is fun. Arony is powerful.";
@@ -14,5 +21,6 @@ public class StringReplace {
 
         String result = replace(s, target, replacement);
         System.out.println(result); 
+        System.out.println(replace("javatpoint is a very good website", 'a', 'e'));
     }
 }
