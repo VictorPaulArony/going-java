c6d98de24178ee7990da366cf5c6b67a9cb44cc9
victorpaularony <smallypauls@gmail.com>
2025-04-09 01:24:30 +0300

    function isPalindrome

--- Palindrome/Palindrome.java
+++ Palindrome/Palindrome.java
@@ -3,6 +3,7 @@ public class Palindrome {
         if (s == null) {
             return false;
         }
+        s = s.toLowerCase();
         String s2 = new StringBuilder(s).reverse().toString();
         return s.equals(s2);
     }
@@ -11,7 +12,9 @@ public class Palindrome {
      public static void main(String[] args) {
         String s1 = "victor";
         String s2 = "omo";
+        String s3 = "AbBa";
         System.out.println(isPalindrome(s1));
         System.out.println(isPalindrome(s2));
+        System.out.println(isPalindrome(s3)); // true
     }
 }
