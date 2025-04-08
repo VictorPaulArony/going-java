6fd783818720ba750fcddbc95ae4d9d73ba5bc4a
victorpaularony <smallypauls@gmail.com>
2025-04-08 17:44:30 +0300

    check If both strings are null, return null

--- StringConcat/StringConcat.java
+++ StringConcat/StringConcat.java
@@ -1,5 +1,9 @@
 public class StringConcat {
     public static String concat(String s1, String s2) {
+        if (s1 == null && s2 == null) {
+            return null;
+        }
+        
         if (s1 == null) {
             s1 = "";
         }
