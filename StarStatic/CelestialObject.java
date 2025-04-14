1d1c9a2473d17d65c01a8e52bafde0da8b18d834
victorpaularony <smallypauls@gmail.com>
2025-04-15 00:27:19 +0300

    ADD: the setter and getter constractor

--- StarStatic/CelestialObject.java
+++ StarStatic/CelestialObject.java
@@ -20,6 +20,35 @@ public class CelestialObject {
         this.name = n;
     }
 
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getZ() {
+        return z;
+    }
+    public void setZ(double z) {
+        this.z = z;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
 
     public static double getDistanceBetween(CelestialObject obj1, CelestialObject obj2) {
         double dx = obj2.x - obj1.x;
