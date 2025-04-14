0c73895ca70b05549b3b0047dda2a35a56e78c32
victorpaularony <smallypauls@gmail.com>
2025-04-15 01:44:50 +0300

    StarUtils/CelestialObject.java

--- StarUtils/CelestialObject.java
+++ StarUtils/CelestialObject.java
@@ -48,6 +48,17 @@ public class CelestialObject {
     public void setName(String name) {
         this.name = name;
     }
+    public static double getDistanceBetween(CelestialObject obj1, CelestialObject obj2) {
+        double dx = obj2.x - obj1.x;
+        double dy = obj2.y - obj1.y;
+        double dz = obj2.z - obj1.z;
+        return Math.sqrt(dx * dx + dy*dy + dz*dz);
+
+    }
+
+    public static double getDistanceBetweenInKm(CelestialObject obj1, CelestialObject obj2) {
+        return getDistanceBetween(obj1, obj2) * KM_IN_ONE_AU;
+    }
 
     @Override
     public String toString() {
