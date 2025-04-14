47bba7008d14e71323b6f56d5d1a89232d37cabe
victorpaularony <smallypauls@gmail.com>
2025-04-14 22:48:15 +0300

    public class named CelestialObject

---
+++ StarProperties/CelestialObject.java
@@ -0,0 +1,14 @@
+public class CelestialObject {
+    double x ;
+    double y;
+    double z ;
+    String name ; 
+
+    public static void main(String[] args) {
+        CelestialObject celestialObject = new CelestialObject();
+        System.out.println(celestialObject.x);
+        System.out.println(celestialObject.y);
+        System.out.println(celestialObject.z);
+        System.out.println(celestialObject.name);
+    }
+}
