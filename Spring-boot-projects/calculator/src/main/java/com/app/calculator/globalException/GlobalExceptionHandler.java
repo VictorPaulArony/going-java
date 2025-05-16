cbd9f0426e5b4c94912d94fae2230353df1f7fa2
victorpaularony <smallypauls@gmail.com>
2025-05-16 21:32:20 +0300

    globalException/GlobalExceptionHandler.java

---
+++ Spring-boot-projects/calculator/src/main/java/com/app/calculator/globalException/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.example.calculator.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ArithmeticException.class)
+    public ResponseEntity<String> handleArithmeticException(ArithmeticException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGenericException(Exception ex) {
+        return new ResponseEntity<>("An error occurred: " + ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
