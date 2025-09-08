b402b060ee6cde15d4dd8daf0e64d749b4fab1c0
victorpaularony <smallypauls@gmail.com>
2025-09-08 23:45:11 +0300

    chore: remove blogplatform Spring Boot project

--- Spring-boot-projects/blogplatform/src/main/java/com/example/blogplatform/repository/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.blogplatform.repository;
-
-import com.example.blogplatform.model.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface CategoryRepository extends JpaRepository<Category, Long>{
-
-}
