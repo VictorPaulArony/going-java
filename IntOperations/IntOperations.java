4f756814a1a0f412a7a131528bd8b6190495eab3
victorpaularony <victorpaularony@gmail.com>
2025-11-03 22:38:41 +0300

    refactor: streamline arithmetic methods and update main method for clarity

--- IntOperations/IntOperations.java
+++ IntOperations/IntOperations.java
@@ -1,28 +1,32 @@
 public class IntOperations {
     public static int addTwoIntegers(int a, int b) {
-        return a + b;
+        return a+b;
     }
     public static int subtractTwoIntegers(int a, int b) {
-        return a - b;
+        return a-b;
     }
     public static int multiplyTwoIntegers(int a, int b) {
-        return a * b;
+        return a*b;
     }
     public static int divideTwoIntegers(int a, int b) {
-        if (b == 0){
-            throw new ArithmeticException("Division by 0: ");
-        }
-        return a / b;
+        return a/b;
     }
-    //main method to test 
-    public static void main(String[] args) {
-        int  a = 10;
-        int b = 2;
 
-        System.out.println("Addition: " + addTwoIntegers(a, b));
-        System.out.println("Subtraction: " + subtractTwoIntegers(a, b));
-        System.out.println("Multiplication: " + multiplyTwoIntegers(a, b));
-        System.out.println("Division: "+ divideTwoIntegers(a, b));
+     public static void main(String[] args) {
+        System.out.println("Add");
+        System.out.println(IntOperations.addTwoIntegers(1, 2));
+        System.out.println(IntOperations.addTwoIntegers(15, 68));
+
+        System.out.println("Subtract");
+        System.out.println(IntOperations.subtractTwoIntegers(4, 1));
+        System.out.println(IntOperations.subtractTwoIntegers(10, 23));
+
+        System.out.println("Multiply");
+        System.out.println(IntOperations.multiplyTwoIntegers(3, 6));
+        System.out.println(IntOperations.multiplyTwoIntegers(12, 11));
+
+        System.out.println("Divide");
+        System.out.println(IntOperations.divideTwoIntegers(8, 2));
+        System.out.println(IntOperations.divideTwoIntegers(13, 4));
     }
-   
 }
