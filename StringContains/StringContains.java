84dc99dd71e8dacf90ea96a7df059f04767707a0
victorpaularony <victorpaularony@gmail.com>
2025-11-03 23:26:26 +0300

    refactor: remove null checks in isStringContainedIn method for simplified logic

--- StringContains/StringContains.java
+++ StringContains/StringContains.java
@@ -1,20 +1,11 @@
 public class StringContains {
     public static boolean isStringContainedIn(String subString, String s) {
-        if (subString == null || s == null) {
-            return false;
-        }
         return s.contains(subString);
     }
 
-    //tester
     public static void main(String[] args) {
-        String subString = "hell";
-        String s = "hello";
-
-        String s1 = "Bonjour";
-        String s2 = "hello World !";
-
-        System.out.println(isStringContainedIn(subString, s));
-        System.out.println(isStringContainedIn(s1, s2));
+        System.out.println(StringContains.isStringContainedIn("Hell", "Highway to Hell"));
+        System.out.println(StringContains.isStringContainedIn("Hell", "Hello World !"));
+        System.out.println(StringContains.isStringContainedIn("Bonjour", "hello World !"));
     }
 }
