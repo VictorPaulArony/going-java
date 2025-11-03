9e71adfa805d4a003762b0406eedf4c683e3bdd0
victorpaularony <victorpaularony@gmail.com>
2025-11-03 22:57:05 +0300

    refactor: improve getActionBeatenBy method and update main method output

--- Chifoumi/Chifoumi.java
+++ Chifoumi/Chifoumi.java
@@ -1,22 +1,18 @@
 public class Chifoumi {
     public static ChifoumiAction getActionBeatenBy(ChifoumiAction chifoumiAction) {
-       switch(chifoumiAction) {
-        case ROCK:
-            return ChifoumiAction.SCISSOR;
-        case PAPER:
-            return ChifoumiAction.ROCK;
-        case SCISSOR:
-            return ChifoumiAction.PAPER;
-        default:
-            throw new IllegalArgumentException("Invalid Action" + chifoumiAction);
-       }
-
+        switch (chifoumiAction) {
+            case ROCK:
+                return ChifoumiAction.SCISSOR;
+            case PAPER:
+                return ChifoumiAction.ROCK;
+            default:
+                return ChifoumiAction.PAPER;
+        }
     }
 
     public static void main(String[] args) {
-        System.out.println("ROCK beats: " + getActionBeatenBy(ChifoumiAction.ROCK));     // SCISSOR
-        System.out.println("PAPER beats: " + getActionBeatenBy(ChifoumiAction.PAPER));   // ROCK
-        System.out.println("SCISSOR beats: " + getActionBeatenBy(ChifoumiAction.SCISSOR)); // PAPER
-
+        System.out.println(Chifoumi.getActionBeatenBy(ChifoumiAction.ROCK));
+        System.out.println(Chifoumi.getActionBeatenBy(ChifoumiAction.PAPER));
+        System.out.println(Chifoumi.getActionBeatenBy(ChifoumiAction.SCISSOR));
     }
 }
