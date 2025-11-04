c6e6939ef51500767af4e92f8e0feaa6327f08a9
victorpaularony <victorpaularony@gmail.com>
2025-11-04 23:52:00 +0300

    refactor: add main method to demonstrate computeArray functionality

--- ComputeArray/ComputeArray.java
+++ ComputeArray/ComputeArray.java
@@ -16,5 +16,11 @@ public class ComputeArray {
         return res;
     }
 
+    public static void main(String[] args) {
+        int[] array = ComputeArray.computeArray(new int[]{9, 13, 8, 23, 1, 0, 89});
+        for (int i : array) {
+            System.out.print(i + " ");
+        }
+    }
    
 }
