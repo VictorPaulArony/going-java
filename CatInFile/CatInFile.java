bac3fa9943d427226511e17e5bb0dcfdf990465d
victorpaularony <victorpaularony@gmail.com>
2025-11-05 16:07:41 +0300

    refactor: update CatInFile.java to a simple and easy to understand code

--- CatInFile/CatInFile.java
+++ CatInFile/CatInFile.java
@@ -1,23 +1,32 @@
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class CatInFile {
     public static void cat(String[] args) throws IOException {
-        if (args.length != 1) {
-            System.err.println("Usage: java CatInFile <output_filename>");
+        if (args == null || args.length == 0) {
             return;
         }
 
-        String filename = args[0];
+        String file = args[0];
 
-        try (
-            InputStream in = System.in;
-            OutputStream out = new FileOutputStream(filename)
-        ) {
-            byte[] buffer = new byte[1024];
-            int bytesRead;
-            while ((bytesRead = in.read(buffer)) != -1) {
-                out.write(buffer, 0, bytesRead);
+        try (InputStream input = System.in; OutputStream out = new FileOutputStream(file)) {
+            int b;
+            while ((b = input.read()) != -1) {
+                out.write(b);
             }
+        } catch (Exception e) {
+            return;
         }
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        String input = "input file test\n";
+        ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        CatInFile.cat(new String[] { "output" });
+        String fileContent = new String(Files.readAllBytes(Paths.get("output")));
+        System.out.println(fileContent.equals(input));
     }
 }
