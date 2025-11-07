b12f90684c870cd8e90142bf37cfa78dde4c1311
victorpaularony <smallypauls@gmail.com>
2025-11-07 23:00:26 +0300

    refactor: remove main method from CelestialObject class

--- CelestialObject/CelestialObject.java
+++ CelestialObject/CelestialObject.java
@@ -3,11 +3,4 @@ public class CelestialObject {
     public double y;
     public double z;
     public String name;
-
-    public CelestialObject() {
-        this.x = 0.0;
-        this.y = 0.0;
-        this.z = 0.0;
-        this.name = null;
-    }
 }
