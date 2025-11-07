c24e21ebdbbd800ac3cf5992c34d3e274ea93563
victorpaularony <smallypauls@gmail.com>
2025-11-07 22:39:42 +0300

    refactor(CelestialObject): initialize fields in constructor instead of declaration

--- CelestialObject/CelestialObject.java
+++ CelestialObject/CelestialObject.java
@@ -2,6 +2,12 @@ public class CelestialObject {
     public double x;
     public double y;
     public double z;
-    public String name = null;
-}
+    public String name;
 
+    public CelestialObject() {
+        this.x = 0.0;
+        this.y = 0.0;
+        this.z = 0.0;
+        this.name = null;
+    }
+}
