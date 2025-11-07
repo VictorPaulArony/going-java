03a84288a8e3249f0a160f0c0a9bd583d84b82d5
victorpaularony <smallypauls@gmail.com>
2025-11-07 23:07:20 +0300

    refactor: remove main method from CelestialObjects class

--- StarStatic/CelestialObjects.java
+++ StarStatic/CelestialObjects.java
@@ -62,13 +62,5 @@ public class CelestialObjects {
         return getDistanceBetween(obj1, obj2) * KM_IN_ONE_AU;
     }
 
-    public static void main(String[] args) {
-        CelestialObject defaultStar = new CelestialObject();
-        CelestialObject earth = new CelestialObject("Terre", 1.0, 2.0, 2.0);
-
-        System.out.println(CelestialObject.getDistanceBetween(defaultStar, earth));
-        System.out.println(CelestialObject.getDistanceBetweenInKm(defaultStar, earth));
-        System.out.println(CelestialObject.KM_IN_ONE_AU);
-    }
 }
 
