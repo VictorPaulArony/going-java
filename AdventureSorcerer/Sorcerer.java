d0d612ee652e6cb07dfeb058deb639ef9ce333b1
victorpaularony <victorpaularony@gmail.com>
2025-11-12 17:05:49 +0300

    feat: add demo main method to Sorcerer class

--- AdventureSorcerer/Sorcerer.java
+++ AdventureSorcerer/Sorcerer.java
@@ -31,4 +31,16 @@ public class Sorcerer extends Character implements Healer {
         return s.append(super.getName()).append(" is a dead sorcerer. So bad, it could heal ").append(healCapacity)
                 .append(" HP.").toString();
     }
+
+    public static void main(String[] args) {
+        Sorcerer gandalf = new Sorcerer("Gandalf", 20, 5);
+        Character frodon = new Character("Frodon", 20);
+        Sorcerer saroumane = new Sorcerer("saroumane", 10, 3);
+
+        Character.fight(frodon, saroumane);
+        
+        gandalf.heal(frodon);
+
+        System.out.println(Character.printStatus());
+    }
 }
