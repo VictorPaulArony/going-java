581548585119aa9af2213badc31d62cf24dc9fc6
victorpaularony <victorpaularony@gmail.com>
2025-11-18 12:19:30 +0300

    refactor: simplify list equality check by removing size comparison

--- ListEquals/ListEquals.java
+++ ListEquals/ListEquals.java
@@ -2,9 +2,6 @@ import java.util.List;
 
 public class ListEquals {
     public static boolean areListsEqual(List<String> list1, List<String> list2) {
-        if (list1.size() != list2.size()) {
-            return false;
-        }
         if (list1 == null && list2 == null) {
             return true;
         }
